fb1d431d3efe9ad659aed154a3d5d11a50f34d81
imtithal <halaloimtithal@gmail.com>
2024-05-22 00:53:39 +0300

    adding loop for transactions

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,12 +1,24 @@
 <template>
     <h3>History</h3>
       <ul id="list" class="list">
-        <li class="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-
-        <li class="plus">
-          Paycheck <span>$400</span><button class="delete-btn">x</button>
+        <li :class="transaction.amount < 0 ? 'minus' : 'plus' " v-for="transaction in transactions" :key="transaction.id">
+          {{ transaction.text }} <span>${{ transaction.amount }}</span><button class="delete-btn">x</button>
         </li>
       </ul>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: 'Flower', amount: -19.99 },
+        { id: 2, text: 'Flower', amount: 19.99 },
+        { id: 3, text: 'Flower', amount: -19.99 },
+        { id: 4, text: 'Flower', amount: 19.99 },
+      ]
+    }
+  },
+}
+</script>
