5747f6dee3fa67978731d3c2eb3d03af36509a70
imtithal <halaloimtithal@gmail.com>
2024-05-22 15:46:29 +0300

    handle delete transaction

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,7 +2,8 @@
     <h3>History</h3>
       <ul id="list" class="list">
         <li :class="transaction.amount < 0 ? 'minus' : 'plus' " v-for="transaction in transactions" :key="transaction.id">
-          {{ transaction.text }} <span>${{ transaction.amount }}</span><button class="delete-btn">x</button>
+          {{ transaction.text }} <span>${{ transaction.amount }}</span>
+          <button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
         </li>
       </ul>
 </template>
@@ -16,4 +17,10 @@
       required: true
     }
   })
+
+  const emit = defineEmits(['transactionDeleted'])
+
+  const deleteTransaction = (id) => {
+    emit('transactionDeleted', id)
+  }
 </script>
